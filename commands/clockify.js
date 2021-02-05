0fda1276a4f314ab9303191c996bb342237b18c1
guijun13 <guilhermejun13.grillo@gmail.com>
2021-02-04 23:50:19 -0300

    :bug: Change clockify post request
    Axios post request for add a new clockify entry was not working, changed for node-fetch

--- commands/clockify.js
+++ commands/clockify.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const fetch = require('node-fetch');
 
 const key = process.env.CLOCKTOKEN;
 const url = `https://api.clockify.me/api/v1`;
@@ -32,20 +33,39 @@ module.exports = {
     })
     console.log(`Num: ${allUsers.data.length}`);
 
-    const newTask = await axios.post(`${url}/workspaces/${workspaceInfo.data[1].id}/time-entries`,{
-      headers: {
-        'X-Api-Key': key,
-        'content-type': 'application/json',
-        'User-Agent': 'Mozilla',
-        'Cookie': 'troute=t1',
-      },
-      body: {
-        start: "2021-02-02T17:00:00.000Z",
-        description: "teste vscode",
-        end: "2021-02-02T18:00:00.000Z"
-      }
-    })
+    // const newTask = await axios.post(`${url}/workspaces/${workspaceInfo.data[1].id}/time-entries`,{
+    //   headers: {
+    //     'X-Api-Key': key,
+    //     'content-type': 'application/json',
+    //     'User-Agent': 'Mozilla',
+    //     'Cookie': 'troute=t1',
+    //   },
+    //   body: {
+    //     start: "2021-02-02T17:00:00.000Z",
+    //     description: "teste vscode",
+    //     end: "2021-02-02T18:00:00.000Z"
+    //   }
+    // })
 
-    console.log(newTask);
+    // console.log(newTask);
+
+    let body = {
+      description: `${arg.join(' ')}`
+    }
+    try {
+      const response = await fetch(`${url}/workspaces/${workspaceInfo.data[1].id}/time-entries`, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "X-Api-Key": key,
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      });
+      // console.log(response);
+      receivedMessage.channel.send(`Tarefa \`${arg.join(' ')}\` iniciada no Clockify`);
+    } catch (err) {
+      console.error(err);
+    }
+
   },
 }
